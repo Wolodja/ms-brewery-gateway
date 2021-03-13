cc1e99cd45b58202e1706c02156929978e87191d
Wolodja <wolodjawaskovych@i.ua>
2021-03-13 19:51:51 +0100

    Added Route for Order Service on Local Host. Closes #3

--- src/main/java/com/sfg/gateway/config/LocalHostRouteConfig.java
+++ src/main/java/com/sfg/gateway/config/LocalHostRouteConfig.java
@@ -14,6 +14,9 @@ public class LocalHostRouteConfig {
             .route("beer-service",
                 r -> r.path("/api/v1/beer", "/api/v1/beer/*", "/api/v1/beerUpc/*")
                     .uri("http://localhost:8080"))
+            .route("order-service",
+                r -> r.path("/api/v1/customers", "/api/v1/customers/**")
+                    .uri("http://localhost:8081"))
             .build();
     }
 }
